cfba8f2d0c584ae5ad1e847c85117d460b2456e9
arieltol <44999484+arieltol@users.noreply.github.com>
2021-02-15 16:54:03 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -4,43 +4,58 @@ import pandas as pd
 from pma_python import core
 core.connect()    #Connection à PMA.start
 
-def print_dict (dict):
-    for nom in dict.keys(): #print toutes les nom des dict 
-        print(nom, "=", dict[nom])
-
-
+#Definition des éléments
 nomfich = "C:/usr/pma_python-master/pma_python/H07-16592-1.02.ndpi"
-#nomfich1_vsi = "D:/Data/VSISampleFilePackage.v2.6/MIA_Multilayer_24bit_RGB_JPEG.vsi"
+infos = core.get_slide_info(nomfich)
+img_tile = [core.get_tile(nomfich, x =x, zoomlevel = x) for x in range (7)] #liste des images selon le zoom 
 
-
-img_tile = [core.get_tile(nomfich, x =x, zoomlevel = x) for x in range (7)] #liste des images selon le zoom  ##aranger ca avec la resoo!!!!
-level_infos = []
-
+#Affiche la tuile avec le zoom
 for i in range(1, 7): #parcourt les elements de la liste à ploter 
     plt.subplot(1,7,i)
     plt.imshow(img_tile[i-1])  
-
 num_lev = len(core.get_zoomlevels_dict(nomfich))
 plt.show()
-print("Voici la fiche")
-barcode = core.get_barcode_image(nomfich)
-barcode.show()
 
 
-info = core.get_slide_info(nomfich)
-print_dict(info)
-#print_dict( info{"MetaData"})
+#Fonction qui print tous les noms des elements de dict
+def print_dict (dict):
+    for nom in dict.keys():  
+        print(nom, "=", dict[nom])
+
+#Fonction pour définir la liste et afficher les MetaData sous forme matricielle contenant name et value
+def print_list(li):
+    for nom in li:
+        print(nom)  
+print("Voici la liste des MetaData")
+print_list(infos["MetaData"])
 
 
-#get_max_zoomlevel(nomfich, sessionID=None)
+print("\nVoici la fiche") 
+print_dict(infos)
+
+
+#Plot le barcode
+#barcode = core.get_barcode_image(nomfich)
+#barcode.show()
+
+print("\nVoici mon truc", infos["MetaData"][2]) #print seulement SlideInfo
+print(type(infos["MetaData"][0])) 
+
+#Print sous forme de ligne les valeurs de SlideInfo en decoupant par value et key
+slideInfoDict = {}
+for i in range(len(infos["MetaData"])):
+     if infos["MetaData"][i]['Name'] == "SlideInfo":
+       slideInfoDict = infos["MetaData"][i]
+       break
+a = slideInfoDict['Value'].split("\r\n") #print sous forme de ligne avec separateur
+print(a)
 
 
 
 
-
-df_levels = pd.DataFrame(level_infos, columns=["res_x", "res_y", "tiles_x", "tiles_y", "approx_mag", "exact_mag", "micropp"])
-print(nomfich)
-print(df_levels)
+#df_levels = pd.DataFrame(level_infos, columns=["res_x", "res_y", "tiles_x", "tiles_y", "approx_mag", "exact_mag", "micropp"])
+#print(nomfich)
+#print(df_levels)
 
 tile_sz = core.get_number_of_tiles(nomfich, zoomlevel = 1) # zoomlevel 1
 for xTile in range(0, tile_sz[0]): #parcourt une liste d'element 
@@ -65,35 +80,3 @@ for xTile in range(0, tile_sz[0]): #parcourt une liste d'element
 #def len_dims():
 #    dims = core.get_zoomlevels_dict(slide_ref) #liste complete pour chaque level 
  #   print(len(dims))
-
-    
-#def __init__(
- #       self, width=None, height=None, tile_size=254, tile_overlap=1, tile_format="jpg"
-  #  ):
-   #     self.width = width
-    #    self.height = height
-     #   self.tile_size = tile_size
-      #  self.tile_overlap = tile_overlap
-       # self.tile_format = tile_format
-        #self._num_levels = None
-
-#def num_levels(self):
- #   """Number of levels in the pyramid."""
-  #  if self._num_levels is None:
-   #     max_dimension = max(self.width, self.height)
-    #    self._num_levels = int(math.ceil(math.log(max_dimension, 2))) + 1
-    #return self._num_levels
-
-#def get_scale(self, level):
- #   """Scale of a pyramid level."""
-  #  assert 0 <= level and level < self.num_levels, "Invalid pyramid level"
-   # max_level = self.num_levels - 1
-    #return math.pow(0.5, max_level - level)
-
-#def get_dimensions(self, level):
- #   """Dimensions of level (width, height)"""
-  #  assert 0 <= level and level < self.num_levels, "Invalid pyramid level"
-   # scale = self.get_scale(level)
-    #width = int(math.ceil(self.width * scale))
-    #height = int(math.ceil(self.height * scale))
-    #return (width, height)
